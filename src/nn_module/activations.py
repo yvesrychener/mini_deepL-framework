fixed tanh backpropagation

src/nn_module/activations.py
@@ -6,15 +6,19 @@
 from . import module
 import torch
 
-# tanh (sigmoid) activation function
+# tanh activation function
 class tanh(module.Module):
+    def __init__(self):
+        super(tanh, self).__init__()
+        self.inputmemory = 0
     # forward pass
     def forward(self, input):
+        self.inputmemory = input
         return torch.tanh(input)
 
     # backward pass
     def backward(self, gradwrtoutput):
-        return 1 - torch.pow(torch.tanh(gradwrtoutput), 2)
+        return 4 * (x.exp() + x.mul(-1).exp()).pow(-2) * gradwrtoutput
 
 
 # relu activation function