10bee79fa18d322eb04de0e4e06afd7bf22bb165
Tobias Barblan <tobias.barblan@epfl.ch>
2019-05-08 15:30:30 +0200

    Cross-Entropy Loss implemented but dloss() yields NaN after just a few iterations and I couldn't figure out why yet.

--- src/nn_module/loss.py
+++ src/nn_module/loss.py
@@ -14,3 +14,21 @@ class MSE(object):
     # derivative of the loss with respect to network output
     def dloss(self, output, target):
         return 2*(output - target)
+
+# Cross-Entropy loss function
+class CE(object):
+    # Calculate the mean cross-entropy loss
+    def loss(self, output, target):
+        return -torch.log(
+            output[:, target.argmax(dim=1)].exp() / output.exp().sum(dim=1)
+        ).mean()
+
+    # derivative of the loss with respect to network output
+    def dloss(self, output, target):
+        var = -(
+            torch.exp(output - output[:, target.argmax(dim=1)]).sum()
+            * torch.exp(output - output[:, target.argmax(dim=1)])
+        )
+        var[:, target.argmax(dim=1)] = 0
+        return var
+
