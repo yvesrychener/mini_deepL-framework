85faaeef2ddc4c4ad8b0e8c5ea2e96a781f9947e
Tobias Barblan <tobias.barblan@epfl.ch>
2019-05-10 23:22:31 +0200

    Fixed the loss value that kept increasing during training.

--- src/nn_module/loss.py
+++ src/nn_module/loss.py
@@ -20,12 +20,14 @@ class CE(object):
     # Calculate the mean cross-entropy loss
     def loss(self, output, target):
         return -torch.log(
-            output[:, target.argmax(dim=1)].exp() / output.exp().sum(dim=1)
+            output[range(output.size(0)), target.argmax(dim=1)].exp()
+            /
+            output.exp().sum(dim=1).view(-1,1)
         ).mean()
 
     # derivative of the loss with respect to network output
     def dloss(self, output, target):
-        var = output.exp() / output.exp().sum(dim=1)
-        var[:, target.argmax(dim=1)] -= 1
-        return var
+        var = output.exp() / output.exp().sum(dim=1).view(-1, 1)
+        var[range(output.size(0)), target.argmax(dim=1)] -= 1
+        return var.mean(dim=0).view(1,-1)
 
